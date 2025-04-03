5a8aefc85c828bd17a38c11055ffe5843906cc35
pedro <pedrohecan@gmail.com>
2025-04-02 22:43:54 -0300

    Entity payment

--- src/main/java/com/pedroestudando/estudo/config/TestConfig.java
+++ src/main/java/com/pedroestudando/estudo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.pedroestudando.estudo.entities.Category;
 import com.pedroestudando.estudo.entities.Order;
 import com.pedroestudando.estudo.entities.OrderItem;
+import com.pedroestudando.estudo.entities.Payment;
 import com.pedroestudando.estudo.entities.Product;
 import com.pedroestudando.estudo.entities.User;
 import com.pedroestudando.estudo.entities.enums.OrderStatus;
@@ -84,7 +85,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
-
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 }
