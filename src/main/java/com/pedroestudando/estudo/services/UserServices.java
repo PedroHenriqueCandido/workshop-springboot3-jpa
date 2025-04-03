65d1237ecee067cd003daebbf903e9158e63ac88
pedro <pedrohecan@gmail.com>
2025-04-03 16:43:05 -0300

    Update excpetion

--- src/main/java/com/pedroestudando/estudo/services/UserServices.java
+++ src/main/java/com/pedroestudando/estudo/services/UserServices.java
@@ -13,6 +13,8 @@ import com.pedroestudando.estudo.repositories.UserRepository;
 import com.pedroestudando.estudo.services.exceptions.DatabaseException;
 import com.pedroestudando.estudo.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserServices {
 	
@@ -43,9 +45,13 @@ public class UserServices {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	private void updateData(User entity,User obj) {
